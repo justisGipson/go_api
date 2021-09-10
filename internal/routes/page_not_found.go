b9f70252362df332ddc033a05edda35c4cbb4f5c
Justis <justis710@gmail.com>
2021-09-10 11:29:12 -0400

    404 route/page not found

--- internal/routes/page_not_found.go
+++ internal/routes/page_not_found.go
@@ -1,1 +1,16 @@
 package routes
+
+import "github.com/gofiber/fiber/v2"
+
+func RouteNotFound(a *fiber.App) {
+	// register new route
+	a.Use(
+		func(c *fiber.Ctx) error {
+			// return 404 status + json res
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"error": true,
+				"msg":   "Page not found",
+			})
+		},
+	)
+}
